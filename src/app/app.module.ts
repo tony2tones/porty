9f32a86eb25b36bb4d193119af6a00294ba20535
Anthony <anthonymarques@outlook.com>
2020-03-25 21:14:20 +0200

    need to put this on ice..need to continue with other views i think...not able to apply fade in to floating button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+import { InViewportModule } from "@thisissoon/angular-inviewport";
+
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./component/header/header.component";
 import { ContentService } from "./services/content-service.service";
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './component/content/home/home.component';
-import { AboutMeComponent } from './component/content/about-me/about-me.component';
-import { ContactComponent } from './component/content/contact/contact.component';
-import { ParallaxDirective } from './parallax.directive';
-import { LandingPageComponent } from './component/content/landing-page/landing-page.component';
-import { TitleComponent } from './title/title.component';
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./component/content/home/home.component";
+import { AboutMeComponent } from "./component/content/about-me/about-me.component";
+import { ContactComponent } from "./component/content/contact/contact.component";
+import { ParallaxDirective } from "./parallax.directive";
+import { LandingPageComponent } from "./component/content/landing-page/landing-page.component";
+import { TitleComponent } from "./title/title.component";
 
 const appRoutes: Routes = [
-  {path: '', component: HomeComponent },
-  {path: 'About', component: AboutMeComponent },
-  {path: 'Contact', component: ContactComponent }
-
-]
+  { path: "", component: HomeComponent },
+  { path: "About", component: AboutMeComponent },
+  { path: "Contact", component: ContactComponent }
+];
 
 @NgModule({
   declarations: [
@@ -31,9 +32,12 @@ const appRoutes: Routes = [
     LandingPageComponent,
     TitleComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(
-    appRoutes
-  )],
+  imports: [
+    BrowserModule,
+    InViewportModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+  ],
   providers: [ContentService],
   bootstrap: [AppComponent]
 })
